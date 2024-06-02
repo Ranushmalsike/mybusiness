0c085b9461748e5e643156ba0d66e955639b681e
Ranushmalsike <ranushmalsika@gmail.com>
2024-06-02 12:12:01 +0530

    06/01/24

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Redirect root to /home
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.redirect('/home');
 });
 
+// Route for /home
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Route for /login
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
